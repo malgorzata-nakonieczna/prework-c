abdc77253b3b246614ec66cd8360504f96d1fd78
Małgorzata Nakonieczna <m.k.nakonieczna@gmail.com>
2020-12-02 21:25:37 +0100

    Integral and floating point numbers

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+namespace NumbersInCSharp
+{
+    class Program
+    {
+        static void WorkingWithIntegers()
+        {
+            int a = 18;
+            int b = 6;
+
+            // addition
+            int c = a + b;
+            Console.WriteLine(c);
+
+            // subtraction
+            c = a - b;
+            Console.WriteLine(c);
+
+            // multiplication
+            c = a * b;
+            Console.WriteLine(c);
+
+            // division
+            c = a / b;
+            Console.WriteLine(c);
+        }
+
+        static void OrderPrecedence()
+        {
+            int a = 5;
+            int b = 4;
+            int c = 2;
+            int d = a + b * c;
+            Console.WriteLine(d);
+
+
+            d = (a + b) * c;
+            Console.WriteLine(d);
+
+            d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+            Console.WriteLine(d);
+
+            int e = 7;
+            int f = 4;
+            int g = 3;
+            int h = (e + f) / g;
+            Console.WriteLine(h);
+        }
+
+        static void TestLimits()
+        {
+            int x = 7;
+            int y = 4;
+            int z = 3;
+            int m = (x + y) / z;
+            int n = (x + y) % z;
+            Console.WriteLine($"quotient: {m}");
+            Console.WriteLine($"remainder: {n}");
+
+            int max = int.MaxValue;
+            int min = int.MinValue;
+            Console.WriteLine($"The range of integers is {min} to {max}");
+
+            int what = max + 3;
+            Console.WriteLine($"An example of overflow: {what}");
+            
+        }
+
+        static void WorkWithDoubles ()
+        {
+            double a = 5;
+            double b = 4;
+            double c = 2;
+            double d = (a + b) / c;
+            Console.WriteLine(d);
+            double e = 19;
+            double f = 23;
+            double g = 8;
+            double h = (e + f) / g;
+            Console.WriteLine(h);
+
+            double max = double.MaxValue;
+            double min = double.MinValue;
+            Console.WriteLine($"The range of double is {min} to {max}");
+
+            double third = 1.0 / 3.0;
+            Console.WriteLine(third);
+
+            
+
+            
+        }
+        static void DecimalDecimalDouble()
+        {
+            double a = 1.0;
+            double b = 3.0;
+            Console.WriteLine(a / b);
+
+            decimal c = 1.0M;
+            decimal d = 3.0M;
+            Console.WriteLine(c / d);
+            decimal min = decimal.MinValue;
+            decimal max = decimal.MaxValue;
+            Console.WriteLine($"The range of the decimal type is {min} to {max}");
+            
+        }
+        static void CircleArea()
+        {
+            double x = 2.5;
+            double y = Math.PI;
+            Console.WriteLine(x * x * y);
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            WorkingWithIntegers();
+
+            OrderPrecedence();
+
+            TestLimits();
+
+            WorkWithDoubles();
+
+            DecimalDecimalDouble();
+
+            CircleArea();
+
+        }
+        
+
+
+
+    }
+}
